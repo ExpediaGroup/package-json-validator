90d0b7274df564ccf1d85656c480d31bba900269
Dan Adajian <dadajian@expediagroup.com>
2023-05-08 12:04:17 -0500

    feat: add resolutions rule (#176)

---
+++ test/rules/resolutions.test.ts
@@ -0,0 +1,26 @@
+import {PackageJson} from 'type-fest';
+import {validateResolutions} from '../../src/rules/resolutions';
+import * as core from '@actions/core';
+
+jest.mock('@actions/core');
+
+describe('resolutions', () => {
+    it('should fail when resolutions are present', function () {
+        const packageJson: PackageJson = {
+            dependencies: {},
+            resolutions: {
+                some: 'resolution'
+            }
+        };
+        validateResolutions(packageJson);
+        expect(core.setFailed).toHaveBeenCalled();
+    });
+
+    it('should not fail when resolutions are not present', function () {
+        const packageJson: PackageJson = {
+            dependencies: {}
+        };
+        validateResolutions(packageJson);
+        expect(core.setFailed).not.toHaveBeenCalled();
+    });
+});
