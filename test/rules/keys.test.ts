5495169d92ed8ca780597b35613b8cd729655528
Dan Adajian <dadajian@expediagroup.com>
2024-01-22 13:22:25 -0600

    feat: add scripts validation to keys rule (#253)

--- test/rules/keys.test.ts
+++ test/rules/keys.test.ts
@@ -2,6 +2,7 @@ import {validateKeys} from '../../src/rules/keys';
 import * as core from '@actions/core';
 import { getMultilineInput } from '@actions/core';
 import dupedPackageJson from '../fixtures/duped-package.json';
+import dupedScriptsPackageJson from '../fixtures/duped-scripts-package.json';
 import dedupedPackageJson from '../fixtures/deduped-package.json';
 
 jest.mock('@actions/core');
@@ -13,7 +14,12 @@ describe('keys', () => {
         expect(core.setFailed).toHaveBeenCalled();
     });
 
-    it('should not fail when package.json contains no duplicate keys', () => {
+    it('should fail when package.json contains duplicate keys in scripts', () => {
+        validateKeys(dupedScriptsPackageJson, 'test/fixtures/duped-scripts-package.json');
+        expect(core.setFailed).toHaveBeenCalled();
+    });
+
+    it('should not fail when package.json contains no duplicate keys or scripts', () => {
         validateKeys(dedupedPackageJson, 'test/fixtures/deduped-package.json');
         expect(core.setFailed).not.toHaveBeenCalled();
     });
